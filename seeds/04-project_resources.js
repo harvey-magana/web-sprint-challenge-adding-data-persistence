55fcd05befbe0ca32c10b092b6ba2bb383696a3f
harvey magana <heyharv2012@gmail.com>
2021-02-06 21:58:47 -0800

    Updated the project resources seed file.

--- seeds/04-project_resources.js
+++ seeds/04-project_resources.js
@@ -3,10 +3,6 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('project_resources').insert([
     { project_id: 1, resource_id: 1 },
-    { project_id: 1, resource_id: 2 },
-    { project_id: 1, resource_id: 1 },
-    { project_id: 2, resource_id: 2 },
-    { project_id: 2, resource_id: 1 },
-    { project_id: 2, resource_id: 1 }
+
   ]);
 };
